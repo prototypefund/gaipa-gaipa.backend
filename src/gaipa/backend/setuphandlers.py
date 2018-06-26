0283ea712803365dae5a274fa803f9555a2d5f1b
MrTango2 <md@derico.de>
2018-06-26 17:35:11 +0200

    change port to 7080, add setup code to setuphandler

--- src/gaipa/backend/setuphandlers.py
+++ src/gaipa/backend/setuphandlers.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+
+import logging
+
+
+PACKAGE_NAME = 'gaipa.backend'
+PROFILE_ID = 'profile-gaipa.backend:default'
 
 
 @implementer(INonInstallable)
@@ -16,7 +23,40 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
-
+    logger = logging.getLogger(PACKAGE_NAME)
+    portal = api.portal.get()
+    ptypes = portal.portal_types
+    # pub_quellen = api.content.get('/publikationen-quellen')
+    if 'app' not in portal.objectIds():
+        obj_id = ptypes.constructContent(
+            'GaipaContent',
+            portal,
+            'app',
+            title=u'gaipa - global farmer empowerment community',
+        )
+        app = portal[obj_id]
+        logger.info('Added {0} database'.format(
+            app.absolute_url_path()))
+    if 'cards' not in app.objectIds():
+        obj_id = ptypes.constructContent(
+            'NavAssistantCards',
+            app,
+            'cards',
+            title=u'Navigation Assistant Cards',
+        )
+        cards = portal[obj_id]
+        logger.info('Added {0} database'.format(
+            cards.absolute_url_path()))
+    if 'solutions' not in app.objectIds():
+        obj_id = ptypes.constructContent(
+            'SolutionArticles',
+            app,
+            'solutions',
+            title=u'Solution Articles',
+        )
+        solutions = portal[obj_id]
+        logger.info('Added {0} database'.format(
+            solutions.absolute_url_path()))
 
 def uninstall(context):
     """Uninstall script"""
